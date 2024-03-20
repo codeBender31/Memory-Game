a614bd96df3158c5798a30a1fc76c2dd536b5cf2
codeBender31 <ibarraisrael949@yahoo.com>
2024-03-20 11:14:52 -0500

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Bender on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
